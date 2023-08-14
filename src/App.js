7131a632ebf8e4abdd07684cf92eba65a057c1c9
T-Luxshan <luckybraveboys@gmail.com>
2023-08-14 12:00:10 +0530

    style modified

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
                     <ul className="cardList">
                         <li className="cardList-item">500 MAUs</li>
                         <li className="cardList-item">3 projects</li>
-                        <li className="cardList-item">Unlimited guides</li>
-                        <li className="cardList-item">Unlimited flows</li>
-                        <li className="cardList-item">Unlimited branded themes</li>
+                        <li className="cardList-item"><s>Unlimited guides</s></li>
+                        <li className="cardList-item"><s>Unlimited flows</s></li>
+                        <li className="cardList-item"><s>Unlimited branded themes</s></li>
                         <li className="cardList-item">Email support</li>
                     </ul>
                 <button className="btn .btn-outline-* choosePlanBtn">Choose Plan</button>
@@ -31,16 +31,16 @@ function App() {
                 <h1 className='cardValue'>$19<span className='cardValue-sub'>/mo</span></h1>
                   
                   <select className='dropDown' name="MAU" id="MAU"> 
-                      <option value="2500MAU">2500MAU</option> 
-                      <option value="300MAU">300MAU</option> 
-                      <option value="4500MAU">4500MAU</option> 
-                      <option value="5000MAU">5000MAU</option> 
+                      <option className='dropdownItem' value="2500MAU">2500MAU</option> 
+                      <option className='dropdownItem' value="300MAU">300MAU</option> 
+                      <option className='dropdownItem' value="4500MAU">4500MAU</option> 
+                      <option className='dropdownItem' value="5000MAU">5000MAU</option> 
                   </select>
                     <ul className="cardList">
-                        <li className="cardList-item">Unlimited guides</li>
-                        <li className="cardList-item">Unlimited flows</li>
-                        <li className="cardList-item">Unlimited branded themes</li>
-                        <li className="cardList-item">Email support</li>
+                        <li className="cardList-item">All starter features, plus:</li>
+                        <li className="cardList-item">Unlimited projects</li>
+                        <li className="cardList-item">Unlimited fully customizable themes</li>
+                        <li className="cardList-item">A dedicated Customer Manager</li>
                     </ul>
                 <button className="btn .btn-outline-* choosePlanBtn">Choose Plan</button>
               </div>
@@ -51,12 +51,13 @@ function App() {
                 <h2 className='cardHead'>Enterprise</h2>
                 <h1 className='cardValue'>$79<span className='cardValue-sub'>/mo</span></h1>
                 <ul className="cardList">
-                        <li className="cardList-item">500 MAUs</li>
-                        <li className="cardList-item">3 projects</li>
-                        <li className="cardList-item">Unlimited guides</li>
-                        <li className="cardList-item">Unlimited flows</li>
+               
+                        <li className="cardList-item">All Pro features, plus:</li>
+                        <li className="cardList-item">Unlimited MAUs</li>
+                        <li className="cardList-item">Dedicated environment</li>
+                        <li className="cardList-item">Enterprise account administration</li>
                         <li className="cardList-item">Unlimited branded themes</li>
-                        <li className="cardList-item">Email support</li>
+                        <li className="cardList-item">Premium support and services</li>
                     </ul>
                 <button className="btn .btn-outline-* choosePlanBtn">Choose Plan</button>
               </div>
